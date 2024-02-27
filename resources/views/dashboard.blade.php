af84311e50f22be45ab6d12964c8baafd9893ef6
younes talibi <younessetalibi11@gmail.com>
2024-02-27 13:22:20 +0100

    update

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -98,22 +98,25 @@
                                     class="font-medium text-blue-600 dark:text-blue-500  hover:text-blue-700 hover:underline">
                                     <i class="bi bi-envelope-at-fill"></i>
                                 </a>
-                                <a href="https://wa.me/?text=Hello%0A%0AYou%20are%20invited%20to%20join%20the%20room%20%22{{ $room->name }}%22.%0ARoom%20Link%3A%20{{ urlencode(env('APP_URL').'/room/'.$room->name.'/'.$room->password)}}%0APassword%3A%20{{ $room->password }}%0AThank%20you%21"
+
+
+
+                                <a href="https://wa.me/?text=Hello%0A%0AYou%20are%20invited%20to%20join%20the%20room%20%22{{ $room->name }}%22.%0ARoom%20Link%3A%20{{ urlencode(env('APP_URL').'/room/'.str_replace(' ', '%20', $room->name).'/'.str_replace(' ','%20',$room->password))}}%0APassword%3A%20{{ $room->password }}%0AThank%20you%21"
                                     target="_blank"
                                     class="font-medium text-blue-600 dark:text-blue-500  hover:text-blue-700 hover:underline">
                                     <i class="bi bi-whatsapp"></i>
                                 </a>
-                                <a href="https://web.skype.com/share?url={{ urlencode(env('APP_URL').'/room/'.$room->name.'/'.$room->password) }}"
+                                <a href="https://web.skype.com/share?url={{ urlencode(env('APP_URL').'/room/'.str_replace(' ', '%20', $room->name).'/'.str_replace(' ','%20',$room->password))}}"
                                     target="_blank"
                                     class="font-medium text-blue-600 dark:text-blue-500  hover:text-blue-700 hover:underline">
                                     <i class="bi bi-skype"></i>
                                 </a>
-                                <a href="https://twitter.com/intent/tweet?text=Hello%0A%0AYou%20are%20invited%20to%20join%20the%20room%20%22{{ $room->name }}%22.%0ARoom%20Link%3A%20{{ urlencode(env('APP_URL').'/room/'.$room->name.'/'.$room->password) }}%0APassword%3A%20{{ $room->password }}%0AThank%20you%21"
+                                <a href="https://twitter.com/intent/tweet?text=Hello%0A%0AYou%20are%20invited%20to%20join%20the%20room%20%22{{ $room->name }}%22.%0ARoom%20Link%3A%20{{ urlencode(env('APP_URL').'/room/'.str_replace(' ', '%20', $room->name).'/'.str_replace(' ','%20',$room->password))}}%0APassword%3A%20{{ $room->password }}%0AThank%20you%21"
                                     target="_blank"
                                     class="font-medium text-blue-600 dark:text-blue-500  hover:text-blue-700 hover:underline">
                                     <i class="bi bi-twitter-x"></i>
                                 </a>
-                                <a href="https://www.reddit.com/submit?url=&title=Hello%20You%20are%20invited%20to%20join%20the%20room%20%22{{ $room->name }}%22.%0ARoom%20Link%3A%20{{ urlencode(env('APP_URL').'/room/'.$room->name.'/'.$room->password) }}%0APassword%3A%20{{ $room->password }}%0AThank%20you%21"
+                                <a href="https://www.reddit.com/submit?url=&title=Hello%20You%20are%20invited%20to%20join%20the%20room%20%22{{ $room->name }}%22.%0ARoom%20Link%3A%20{{ urlencode(env('APP_URL').'/room/'.str_replace(' ', '%20', $room->name).'/'.str_replace(' ','%20',$room->password))}}%0APassword%3A%20{{ $room->password }}%0AThank%20you%21"
                                     target="_blank"
                                     class="font-medium text-blue-600 dark:text-blue-500  hover:text-blue-700 hover:underline">
                                     <i class="bi bi-reddit"></i>
